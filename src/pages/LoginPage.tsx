809ae8e0251c4a357c6d03f42137901f8d093fca
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2022-12-07 15:14:08 +0100

    Send user back to home page after logging in

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { Component, ReactNode } from "react";
+import { Navigate } from "react-router-dom";
 import GoogleLoginButton from "../components/buttons/GoogleLoginButton";
 import AccountService from "../services/AccountService";
 import "../styles/LoginPage.css";
@@ -7,15 +8,36 @@ interface Props {
     onUserLogin(user: any, accountService: AccountService): void;
 }
 
-export default class LoginPage extends Component<Props> {
+interface State {
+    redirect: boolean;
+}
+
+export default class LoginPage extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            redirect: false
+        };
+    }
+
     public render(): ReactNode {
+        if (this.state.redirect) {
+            return (<Navigate to="/" />)
+        }
         return (
             <div className="centre">
                 <div className="login-title">Log In</div>
                 <div className="login-container">
-                    <GoogleLoginButton onUserLogin={this.props.onUserLogin} />
+                    <GoogleLoginButton onUserLogin={(u, s) => this.onUserLogin(u, s)} />
                 </div>
             </div>
         );
+    }
+
+    private onUserLogin(user: any, accountService: AccountService): void {
+        this.props.onUserLogin(user, accountService);
+        this.setState({
+            redirect: true
+        });
     }
 }
