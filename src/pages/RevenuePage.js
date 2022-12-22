f40ce3dd360c3e41327f8f7f4baceeababc7048f
Espilonius <435518@student.fontys.nl>
2022-12-22 14:23:22 +0100

    Websockets revenue

--- src/pages/RevenuePage.js
+++ src/pages/RevenuePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "../styles/pages/RevenuePage.module.css";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -7,15 +7,52 @@ import { useState } from "react";
 import { TextField } from "@mui/material";
 import { getRevenue } from "../services/RevenueService";
 
+import { HubConnectionBuilder } from '@microsoft/signalr';
+import axios from 'axios';
+
 const RevenuePage = () => {
   const [date, setDate] = useState();
   const [revenue, setRevenue] = useState(0);
 
+  const latestRevenue = useRef(null);
+
+  latestRevenue.current = revenue;
+
+  const isInitialMount = useRef(true);
+
   useEffect(() => {
     getRevenue(date).then((e) => {
+      console.log(e);
       setRevenue(e.data);
     });
   }, [date]);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+        isInitialMount.current = false;
+        return
+    }
+
+    const connection = new HubConnectionBuilder()
+        .withUrl('https://localhost:7205/hubs/revenue')
+        .withAutomaticReconnect()
+        .build();
+
+    connection.start()
+        .then(result => {
+            console.log('Connected!');
+
+            connection.on('ReceiveRevenue', message => {
+              console.log("Do refresh")                
+
+                console.log('received revenue', message);
+
+                setRevenue(message);
+            });
+        })
+        .catch(e => console.log('Connection failed: ', e));
+    isInitialMount.current = true;
+}, []);
 
   return (
     <>
@@ -42,12 +79,13 @@ const RevenuePage = () => {
               />
             </LocalizationProvider>
             <h1>Total revenue selected day:</h1>
-            <h1>{revenue}</h1>
+            <h1>€{revenue}</h1>
           </div>
         </div>
       </div>
     </>
   );
 };
+
 
 export default RevenuePage;
--- src/services/RevenueService.js
+++ src/services/RevenueService.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import Data from "../App.json";
+import dayjs from "dayjs";
 
 export async function getRevenue(date) {
   try {
-    axios.get(Data.serverAddress + `/api/Receipts/byday/${date}`);
+    https://localhost:7205/api/Receipts/byday
+    return axios.get(Data.ConsumerAddress + `/Receipts/byday/${dayjs(date).format("YYYY-MM-DD")}`);
   } catch (error) {
     console.log(error);
   }
