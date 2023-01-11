79ef94d1c293c6d0027e910dc660f751bcb24dea
Espilonius <435518@student.fontys.nl>
2023-01-11 13:01:56 +0100

    Update RevenueService.js

--- src/services/RevenueService.js
+++ src/services/RevenueService.js
@@ -3,8 +3,7 @@ import Data from "../App.json";
 import dayjs from "dayjs";
 
 export async function getRevenue(date) {
-  try {
-    https://localhost:7205/api/Receipts/byday
+  try {    
     return axios.get(Data.ConsumerAddress + `/Receipts/byday/${dayjs(date).format("YYYY-MM-DD")}`);
   } catch (error) {
     console.log(error);
